e25a41686544f39da0bde7c8f279e16335d3d36b
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-13 17:33:46 -0500

    Parsing out first and last names from .txt file

--- tsh_dx_module.py
+++ tsh_dx_module.py
@@ -1,4 +1,16 @@
 def main:
+    dataFile = open("test_data.txt", "r")
+
+def readFile:
+    dataFile = open("test_data.txt", "r")
+    lineCount = 0
+    for line in dataFile:
+        if line.startswith("END"):
+            break
+        lineCount += 1
+        if lineCount % 4 == 1:
+            firstName = line.rstrip("\n").split(" ")[0]
+            lastName = line.rstrip("\n").split(" ")[1]
 
 
 if __name__ == "__main__":
