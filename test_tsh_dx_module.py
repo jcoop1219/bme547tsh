6484a67b55971d8d714bc810ef8204950d9e642f
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-14 16:47:57 -0500

    Renaming function in parametrize decorator

--- test_tsh_dx_module.py
+++ test_tsh_dx_module.py
@@ -15,16 +15,16 @@ case3 = "hypothyroidism"
 
 
 @pytest.mark.parametrize("diagnosis, expectedDx", [
-                        (personDict[0]["Diagnosis"], case1),
-                        (personDict[1]["Diagnosis"], case2),
-                        (personDict[2]["Diagnosis"], case1),
-                        (personDict[3]["Diagnosis"], case3),
-                        (personDict[4]["Diagnosis"], case2),
-                        (personDict[5]["Diagnosis"], case1),
-                        (personDict[6]["Diagnosis"], case3),
-                        (personDict[7]["Diagnosis"], case2),
-                        (personDict[8]["Diagnosis"], case3),
-                        (personDict[9]["Diagnosis"], case2),
+                        (personDictList[0]["Diagnosis"], case1),
+                        (personDictList[1]["Diagnosis"], case2),
+                        (personDictList[2]["Diagnosis"], case1),
+                        (personDictList[3]["Diagnosis"], case3),
+                        (personDictList[4]["Diagnosis"], case2),
+                        (personDictList[5]["Diagnosis"], case1),
+                        (personDictList[6]["Diagnosis"], case3),
+                        (personDictList[7]["Diagnosis"], case2),
+                        (personDictList[8]["Diagnosis"], case3),
+                        (personDictList[9]["Diagnosis"], case2),
                         ])
 def test_calculateDx(diagnosis, expectedDx):
     assert diagnosis == expectedDx
