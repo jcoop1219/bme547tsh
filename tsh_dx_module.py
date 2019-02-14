75ead9e401140cf15ab8cd1fe5fb158cae6261ae
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-14 00:56:55 -0500

    Parsing age and gender lines out of text file

--- tsh_dx_module.py
+++ tsh_dx_module.py
@@ -5,12 +5,17 @@ def readFile:
     dataFile = open("test_data.txt", "r")
     lineCount = 0
     for line in dataFile:
-        if line.startswith("END"):
+        if line.startswith("END"): # if we're at the end of the file, quit the loop
             break
         lineCount += 1
-        if lineCount % 4 == 1:
+        lineType = lineCount % 4
+        if lineType == 1: # name line
             firstName = line.rstrip("\n").split(" ")[0]
             lastName = line.rstrip("\n").split(" ")[1]
+        elif lineType == 2: # age line
+            age = int(line.rstrip("\n"))
+        elif lineType == 3: # gender line
+            gender = line.rstrip("\n")
 
 
 if __name__ == "__main__":
