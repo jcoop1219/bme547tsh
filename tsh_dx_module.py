33356c023f6abaf75b2804b242ef4d9502582782
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-14 02:27:17 -0500

    Adding spacing to comments to meet PEP8 guidelines

--- tsh_dx_module.py
+++ tsh_dx_module.py
@@ -2,30 +2,31 @@ def main():
     fileName = "test_data.txt"
     personDict = createDict(fileName)
 
+
 def createDict(fileName):
     dataFile = open(fileName, "r")
     lineCount = 0
     personNum = -1
     personDict = []
     for line in dataFile:
-        if line.startswith("END"): # if we're at the end of the file, quit the loop
+        if line.startswith("END"):  # if we're at the end of the file, quit
             break
         lineCount += 1
         lineType = lineCount % 4
-        if lineType == 1: # name line
+        if lineType == 1:  # name line
             personNum += 1
             personDict.append({})
             firstName = line.rstrip("\n").split(" ")[0]
             lastName = line.rstrip("\n").split(" ")[1]
             personDict[personNum]["First Name"] = firstName
             personDict[personNum]["Last Name"] = lastName
-        elif lineType == 2: # age line
+        elif lineType == 2:  # age line
             age = int(line.rstrip("\n"))
             personDict[personNum]["Age"] = age
-        elif lineType == 3: # gender line
+        elif lineType == 3:  # gender line
             gender = line.rstrip("\n")
             personDict[personNum]["Gender"] = gender
-        else: # TSH line
+        else:  # TSH line
             tshList = line.lstrip("TSH,").rstrip("\n").split(",")
             for idx, item in enumerate(tshList):
                 tshList[idx] = float(item)
